98d8f169751dae9be8765934c795e65ed98806c3
Yanki Muslu <yamuslu@gmail.com>
2017-02-16 13:23:04 +0100

    Minor changes

--- CDIO1/src/persistency/IPersistency.java
+++ CDIO1/src/persistency/IPersistency.java
@@ -1,12 +1,16 @@
 package persistency;
 
+import java.util.List;
+
 import dto.UserDTO;
 
 public interface IPersistency {
-
-	UserDTO load(int usesrID);	
-	boolean save(UserDTO user);	
-	void updateUser(UserDTO user, int userID);
 	
+	UserDTO load(int userID);	
+	boolean save(UserDTO user);
+	void updateUser(UserDTO user, int i);
+	UserDTO getUser(int id);
+	List<UserDTO> getUserList();
+	List<Integer> getUsedIds();
 	
 }
